d7bf9a5a35e3a580ad31f28210111012e2f74874
Andrey Rian <andreyrian581@gmail.com>
2026-09-06 22:02:28 -0300

    feat: Criação de input para senha e criação de Component AuthTabs

--- src/features/auth/RegisterPage.jsx
+++ src/features/auth/RegisterPage.jsx
@@ -1,27 +1,22 @@
 import Navbar from "../../components/layout/Navbar";
+import AuthTabs from "../../components/layout/AuthTabs";
 function RegisterPage() {
 	return (
 		<>
 			<Navbar />
 
-			<div className="container mt-4 ">
+			<div className="container mt-3 ">
 				<h1 className="fw-bold text-primary text-center" style={{ fontSize: '28px' }}>
 					Cadastro Acadêmico
 				</h1>
-				<p className="fw-bold mb-4 text-center" style={{ fontSize: '15px', color: '#7B8A8B' }}>
+				<p className="fw-bold mb-3 text-center" style={{ fontSize: '15px', color: '#7B8A8B' }}>
 					Crie sua conta para avaliar professores e disciplinas
 				</p>
 			</div>
-			<div className="card register-card p-4 mx-auto" style={{ maxWidth: "520px" }}>
+			<div className="card register-card p-4 mx-auto mt-1" style={{ maxWidth: "520px" }}>
 
-				<div className="bg-light w-100 p-1 d-flex rounded " >
-					<button type="button" className=" fw-bold btn btn-success me-1 bg-white w-100 border-0" style={{ color: "#7B8A8B" }}>
-						Acessar Conta
-					</button>
-					<button type="button" className="fw-bold btn btn-success w-100">
-						Criar Nova Conta
-					</button>
-				</div>
+				<AuthTabs page="register"/>
+				
 				<p className="fw-bold my-2 d-flex" style={{ fontSize: '18px', color: '#2C3E50' }}>Dados do Usuário</p>
 				<form action="" className="d-flex flex-column align-items-start">
 					<p className="fw-semibold my-1 " style={{ fontSize: '12px', color: '#2C3E50' }}>Nome Completo</p>
@@ -48,7 +43,15 @@ function RegisterPage() {
 						<button type="button" className=" fw-bold btn btn-success me-1 bg-white w-100 border-1" style={{ color: "#7B8A8B" }}>
 							Sou Professor
 						</button>
+
 					</div>
+					<p className="fw-semibold my-1 " style={{ fontSize: '12px', color: '#2C3E50' }}>Criar Senha de Acesso</p>
+					<input type="text" className="form-control shadow-none" style={{
+						height: '40px',
+						borderRadius: '6px',
+						backgroundColor: '#ffffff',
+						border: '1px solid #ced4da'
+					}} placeholder="••••••••••••" />
 					<p className="fw-semibold" style={{ fontSize: '12px', color: '#18BC9C' }}>✓ Mínimo 8 dígitos   ✓ Letras e números   ✓ E-mail verificado</p>
 
 					<button type="button" className="fw-semibold btn btn-success w-100">
